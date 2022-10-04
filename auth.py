f02acd43e8ca42a3d106c96982443e76e162ed99
anchal00 <anchal.sharma@ridecell.com>
2022-10-04 22:43:53 +0530

    Complete auth flow

--- auth.py
+++ auth.py
@@ -1,15 +1,20 @@
 import base64
 import os
+import webbrowser
 
 import requests
 from dotenv import load_dotenv
 
-authentication_url = "https://accounts.spotify.com/api/token"
+AUTH_URL = "https://accounts.spotify.com"
 
 load_dotenv()
 
+code = None
 client_id = os.getenv("client_id")
 client_secret  = os.getenv("client_secret")
+
+REDIRECT_URI = "http://localhost:8000/callback"
+scopes = ["user-read-private", "user-read-email", "user-modify-playback-state"]
 
 headers = {
     "Content-Type": "application/x-www-form-urlencoded",
@@ -18,8 +23,13 @@ headers = {
 }
 
 def authenticate():
-    response = requests.post(url=authentication_url, 
-                             params={"grant_type": "client_credentials"}, headers=headers)
+    scope_str = " ".join(scopes)
+    response = requests.get(f'{AUTH_URL}/authorize?client_id={client_id}&scope={scope_str}&response_type=code&redirect_uri={REDIRECT_URI}')
+    webbrowser.open(response.url)
+    code = input("""Grant the access to spotify-cli and Enter the code from the url 
+                 \ne.g. If the address shown in the browser's address bar is -> http://localhost:8000/callback?code=abcdef then abcdef is the code that you need to enter: """)
+    response = requests.post(url=f"{AUTH_URL}/api/token", 
+                             params={"grant_type": "authorization_code", "code": code, "redirect_uri": REDIRECT_URI}, headers=headers)
     if response.ok:
         data = response.json()
         return (True, data["access_token"])
