f62740ab04915ceefac306aeef562d5c74a69ef7
anchal00 <anchal.sharma@ridecell.com>
2022-10-07 13:15:59 +0530

    Implement functionality for resuming songs

--- playback.py
+++ playback.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_ENDPOINT = "https://api.spotify.com/v1/me/player"
-
 headers = {
     "Content-Type": "application/json",
     "Authorization": "Bearer " + os.getenv("access_token")
@@ -14,24 +13,31 @@ headers = {
 
 def get_currently_playing_song():
     response = requests.get(url=API_ENDPOINT + "/currently-playing", headers=headers)
-    if response.ok:
-        print("CURRENTLY PLAYING : " + response.json()["item"]["name"])
+    data = response.json()
+    if response.ok and data:
+        if data.get("item"):
+            print("CURRENTLY PLAYING song: " + data["item"]["name"])
+        else:
+            print("CURRENTLY PLAYING an " + data["currently_playing_type"])
     else:
         print("Couldn't get currently playing song info")
 
 def pause_currently_playing_song():
     response = requests.put(url=API_ENDPOINT + "/pause", headers=headers)
-    import IPython
-    IPython.embed()
     if response.ok:
         print("PAUSED the playback")
     else:
         print("FAILED to pause the playback")
 
+def resume_currently_playing_song():
+    response = requests.put(url=API_ENDPOINT + "/play", headers=headers)
+    if response.ok:
+        print("RESUMED the playback")
+    else:
+        print("FAILED to resumed the playback")
+
 def adjust_volume(level):
     response = requests.put(url=API_ENDPOINT + f"/pause?volume_percent={level}", headers=headers)
-    import IPython
-    IPython.embed()
     if response.ok:
         print(f"VOLUME set to {level} percent")
     else:
--- spotify_cli.py
+++ spotify_cli.py
@@ -3,24 +3,23 @@ import sys
 
 import auth
 from playback import (adjust_volume, get_currently_playing_song,
-                      pause_currently_playing_song)
+                      pause_currently_playing_song,
+                      resume_currently_playing_song)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Control spotify playback using your command line")
     subparser = parser.add_subparsers(help="Sub commands for controlling playback")
-
     # Auth controls
     parser.add_argument("--authenticate", help="Authorize with spotify APIs", action="store_true")
-
     # Playback controls
     playback_parser = subparser.add_parser(name="playback", description="Playback controls")
     playback_parser.add_argument("--current", help="Get currently playing song", action="store_true")
     playback_parser.add_argument("--playnext", help="Play next song", action="store_true")
+    playback_parser.add_argument("--resume", help="Resume the currently playing song", action="store_true")
     playback_parser.add_argument("--pause", help="Pause currently playing song", action="store_true")
     playback_parser.add_argument("--volume", help="Adjust the volume of currently playing song", type=int)
-
     args = parser.parse_args()
-    print(args)
+
     if args.authenticate:
         is_successful = auth.authenticate()
         if is_successful == False:
@@ -29,9 +28,11 @@ if __name__ == "__main__":
             sys.exit()
     elif args.current:
         get_currently_playing_song()
+    elif args.resume:
+        resume_currently_playing_song()
     elif args.playnext:
         pass
     elif args.pause:
         pause_currently_playing_song()
     elif args.volume:
-        adjust_volume(30)
+        adjust_volume(args.volume)
