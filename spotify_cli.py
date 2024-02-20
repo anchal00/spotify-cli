c955b41e3657a5f994bfc9a8a75519a2ee4262d4
anchal00 <anchal82199@gmail.com>
2024-02-20 15:44:46 +0530

    Fix cli

--- spotify_cli.py
+++ spotify_cli.py
@@ -5,31 +5,47 @@ import sys
 from spotify_playback.spotify_client import SpotifyClient
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Control spotify playback using your command line")
+    parser = argparse.ArgumentParser(
+        description="Control spotify playback using your command line", add_help=False
+    )
     subparser = parser.add_subparsers(help="Sub commands for controlling playback")
     # Auth controls
-    parser.add_argument("--auth", help="Authorize with spotify APIs", action="store_true")
+    parser.add_argument("-auth", help="Authorize with spotify APIs", action="store_true")
+    parser.add_argument("-h", help="Help", action="store_true")
     # Playback controls
-    playback_parser = subparser.add_parser(name="pbk", description="Playback controls")
-    playback_parser.add_argument("--cr", help="Get currently playing song", action="store_true")
-    playback_parser.add_argument("--ppr", help="Play previous song", action="store_true")
-    playback_parser.add_argument("--pnx", help="Play next song", action="store_true")
-    playback_parser.add_argument("--rs", help="Resume the currently playing song", action="store_true")
-    playback_parser.add_argument("--ps", help="Pause currently playing song", action="store_true")
-    playback_parser.add_argument("--vol", help="Adjust the volume of currently playing song", type=int)
-    playback_parser.add_argument("--s", help="Search for a track, playlist, show etc. \
-                                              Specify as: --search <keyword><space><type>. \
+    playback_parser = subparser.add_parser(name="playback", description="Playback controls", add_help=True)
+    playback_parser.add_argument("-cr", help="Get currently playing song", action="store_true")
+    playback_parser.add_argument("-ppr", help="Play previous song", action="store_true")
+    playback_parser.add_argument("-pnx", help="Play next song", action="store_true")
+    playback_parser.add_argument("-rs", help="Resume the currently playing song", action="store_true")
+    playback_parser.add_argument("-ps", help="Pause currently playing song", action="store_true")
+    playback_parser.add_argument("-vol", help="Adjust the volume of currently playing song", type=int)
+    playback_parser.add_argument("-s", help="Search for a track, playlist, show etc. \
+                                              Specify as: -search <keyword><space><type>. \
                                               <type> could be any of 'track’, ‘playlist’, ‘show’, and ‘episode’",
                                               type=str, nargs='+')
-    playback_parser.add_argument("--dvc", help="Get all your devices", action="store_true")
-    playback_parser.add_argument("--q", help="Get user's queue", action="store_true")
-    playback_parser.add_argument("--qad", help="Add item to the queue", type=str)
-    args = parser.parse_args()
+    playback_parser.add_argument("-dvc", help="Get all your devices", action="store_true")
+    playback_parser.add_argument("-q", help="Get user's queue", action="store_true")
+    playback_parser.add_argument("-qad", help="Add item to the queue", type=str)
+    
+    subparsers_actions = [
+        action for action in parser._actions if isinstance(action, argparse._SubParsersAction)
+    ]
+
+    help_text = "\n" + parser.format_help() 
+    for subparsers_action in subparsers_actions:
+        # get all subparsers and print help
+        for choice, subparser in subparsers_action.choices.items():
+            help_text += f"{'_' * 130}\n\n" + subparser.format_help()
+
     if not os.path.isfile(".env"):
         print("Please create .env file and add your 'client_id' and 'client_secret' to it before proceeding")
         sys.exit()
+    args = parser.parse_args()
     spfy_client = SpotifyClient()
-    if args.auth:
+    if args.h:
+        print(help_text)
+    elif args.auth:
         spfy_client.authenticate()
     elif args.cr:
         spfy_client.get_currently_playing_song()
@@ -42,12 +58,12 @@ if __name__ == "__main__":
     elif args.ps:
         spfy_client.pause_currently_playing_song()
     elif args.vol:
-        spfy_client.adjust_volume(args.volume)
+        spfy_client.adjust_volume(args.vol)
     elif args.s:
-        spfy_client.search(args.search)
+        spfy_client.search(args.s)
     elif args.dvc:
         spfy_client.get_devices()
     elif args.q:
         spfy_client.get_queue()
     elif args.qad:
-        spfy_client.add_to_queue(args.queue_add)
+        spfy_client.add_to_queue(args.qad)
--- spotify_playback/spotify_client.py
+++ spotify_playback/spotify_client.py
@@ -3,11 +3,10 @@ import sys
 
 import spotipy
 from dotenv import load_dotenv
-from spotipy.exceptions import SpotifyException
-from spotipy.oauth2 import SpotifyOAuth
-
 from spotify_playback.utils import (print_album_info, print_playlist_info,
                                     print_track_info)
+from spotipy.exceptions import SpotifyException
+from spotipy.oauth2 import SpotifyOAuth
 
 REDIRECT_URI = "http://localhost:8000/callback"
 
